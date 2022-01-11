9048338a5e37010b39695ccd7398c149b6167f00
Keefer Gregoire <keefergregoire@Keefers-MacBook-Pro.local>
2022-01-11 17:40:51 -0500

    sign in landing page finally

--- src/Components/misc/Footer.js
+++ src/Components/misc/Footer.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import { Col, Footer, Grid, Row } from 'rsuite';
 
-const Footer = ({style}) => (
-        <div style={{...style, padding: '10px 30px', position: 'fixed', bottom: 0, right: 0}}>
-            <p style={{textAlign: 'right'}}>Created by EDP <a href='https://www.linkedin.com/in/keefergregoire/'>Keefer Gregoire</a></p>
-        </div>
-    );
+const MyFooter = ({style}) => (
+    <Footer style={{...style, clear: 'both',
+        position: 'absolute',
+        bottom: '0px',
+        width: '100%',
+        padding: '15px',
+        zIndex: 0
+      }}
+
+        className='light-shade-c'
+      >
+        <Grid fluid>
+          <Row>
+            <Col xs={18}>
+              <h5>
+                SysDescriptor
+              </h5>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={4} lg={2}>
+              <a href='/'>Privacy Policy</a>
+            </Col>
+            <Col xs={4} lg={2}>
+              <a href='/'>Terms of Use</a>
+            </Col>
+            <Col xs={4} lg={2}>
+              <a href='/'>Accessibility</a>
+            </Col>
+            <Col xs={12} lg={18}>
+            <div>
+            <p style={{textAlign: 'right'}}>
+               Developed by Siemen Engineer <a href='https://www.linkedin.com/in/keefergregoire/'>Keefer Gregoire</a>
+             </p>
+            </div>
+            </Col>
+          </Row>
+        </Grid>
+    </Footer>
+);
 
 
-export default Footer;
+export default MyFooter;
